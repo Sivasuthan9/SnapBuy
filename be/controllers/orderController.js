cf88355e7e755b4de82da859a172eb920f92a5e4
Sivasuthan Sukumar <153247727+Sivasuthan9@users.noreply.github.com>
2025-07-25 20:22:00 +0530

    number of stock is handled.

--- be/controllers/orderController.js
+++ be/controllers/orderController.js
@@ -1,13 +1,22 @@
-const OrderModel = require('../models/orderModel');
+const productModel = require('../models/productModel');
+const orderModel = require('../models/orderModel');
 
-exports.createOrder = (req, res, next) => {
-    console.log(req.body+' DATAAAAAAAAA');
+exports.createOrder = async (req, res, next) => {
     const cartItems = req.body;
-    const ammount = cartItems.reduce((acc, item) => (acc+ item.product.price * item.qyt), 0);
-    console.log('Ammountttt----:' + ammount);
+    const ammount = Number(cartItems.reduce((acc, item) => (acc+ item.product.price * item.qty), 0)).toFixed(2);
+    const status = 'pending';
+    const order = await orderModel.create({cartItems, ammount, status});
+
+    cartItems.forEach(async (item) => {
+        const product = await productModel.findById(item.product._id?.$oid || item.product._id); // How ._id works?
+        product.stock = product.stock - item.qty; // stock ammount is handeled.
+        // console.log(product.stock, item.qty);
+        await product.save();
+    });
+
     res.json({
         success: true,
-        message: 'Order Works!'
+        order
     })
 
 }
