a6622722c0b3e8fa9d5c270604b31b4610059032
Sivasuthan Sukumar <153247727+Sivasuthan9@users.noreply.github.com>
2025-07-25 10:11:14 +0530

    connected with local MongoDB

--- be/app.js
+++ be/app.js
@@ -6,11 +6,15 @@ const app = express();
 
 const product =  require('./routes/product');
 const order = require('./routes/order');
+const connectDatabase = require('./config/connectDatabase');
 
 app.use('/api/v1', product);
 app.use('/api/v1', order);
 
 dotenv.config({path: path.join(__dirname, 'config', 'config.env')})
+
+
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening to port ${process.env.PORT} in ${process.env.NODE_ENV}!`)
