813d533fdd1a02f84bf8acab71d1379aa639d9d9
Sivasuthan Sukumar <153247727+Sivasuthan9@users.noreply.github.com>
2025-08-06 02:45:23 +0530

    product-detail page templet added.

--- fe/src/app/app.routes.ts
+++ fe/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';   
 import { Home } from './home/home';
+import { ProductDetail } from './product-detail/product-detail';
 
 export const routes: Routes = [
-    {path: "", component: Home}
+    {path: "", component: Home},
+    {path: "product/:id", component: ProductDetail}
 ];
