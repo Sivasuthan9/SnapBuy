202a664e351942de286fd7f45281ddba73b4d3cb
Sivasuthan Sukumar <153247727+Sivasuthan9@users.noreply.github.com>
2025-08-06 03:13:42 +0530

    BehaviorSubject is used to send the data using next().

--- fe/src/app/api.ts
+++ fe/src/app/api.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment.development';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs'; // ?????
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +10,21 @@ export class Api {
   
   constructor(private http: HttpClient) {}
 
-  getProducts(): Observable<any>{ //TODO: Handle with proper typing
-    return this.http.get(environment.apiUrl+'/api/v1/product')
+  productSource = new BehaviorSubject([]);
+  currentProducts = this.productSource.asObservable(); // ????
+  
+  getProducts(){ //TODO: Handle with proper typing
+    this.http.get(environment.apiUrl+'/api/v1/product').subscribe((data: any) => {
+      this.productSource.next(data)
+    })
   }
 
-  searchProduct(searchText:string): Observable<any>{
-    return this.http.get(environment.apiUrl+'/api/v1/product',
+  searchProduct(searchText:string){
+    this.http.get(environment.apiUrl+'/api/v1/product',
     {
       params: {keyword: searchText}
+    }).subscribe((data: any) => {
+      this.productSource.next(data)
     })
   }
 }
