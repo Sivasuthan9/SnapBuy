e7abaa3c797a7964c65b5a9428c70c912d3a1773
Sivasuthan Sukumar <153247727+Sivasuthan9@users.noreply.github.com>
2025-08-07 00:29:38 +0530

    product-detail pages is dynamic now.

--- fe/src/app/product-detail/product-detail.ts
+++ fe/src/app/product-detail/product-detail.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.css'
 })
 export class ProductDetail {
-
+      product:any = null;
 }
