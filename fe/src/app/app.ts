c6430c5b2b33716635f5d1959aafff86dda50d91
Sivasuthan Sukumar <153247727+Sivasuthan9@users.noreply.github.com>
2025-08-07 01:11:55 +0530

    logo click directs to home

--- fe/src/app/app.ts
+++ fe/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { Api } from './api';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
