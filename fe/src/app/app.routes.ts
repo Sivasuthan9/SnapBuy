f7e576bf05a33537aa97881dc27c6907811af5ef
Sivasuthan Sukumar <153247727+Sivasuthan9@users.noreply.github.com>
2025-08-10 15:37:56 +0530

    OrderSuccess html added.

--- fe/src/app/app.routes.ts
+++ fe/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { ProductDetail } from './product-detail/product-detail';
 import { Cart } from './cart/cart';
+import { OrderSuccess } from './order-success/order-success';
 
 export const routes: Routes = [
     {path: "", component: Home},
     {path: "product/:id", component: ProductDetail},
-    {path: "cart", component: Cart}
+    {path: "cart", component: Cart},
+    {path: "order/success/:id", component: OrderSuccess}
 ];
